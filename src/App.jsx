59cea0fdec3f35274e6c1653aeb859d9df8b513e
rossenahuh <rossenahuh@gmail.com>
2019-05-15 10:25:23 +0900

    completed react-router

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Main from './Main/Main';
+import Login from './Login/Login';
+import SearchResult from './SearchResult/SearchResult';
+import StoreInfo from './StoreInfo/StoreInfo';
+import HeartedItems from './HeartedItems/HeartedItems';
+import NoMatch from './404NotFound';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/login" component={Login} />
+          <Route
+            path="/searchresult/:userid/:countrycode/:storecode/:category"
+            component={SearchResult}
+          />
+          <Route
+            path="/storeinfo/:countrycode/:storecode"
+            component={StoreInfo}
+          />
+          <Route path="/hearteditems/:userid" component={HeartedItems} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
