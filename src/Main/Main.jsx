22c8945acd10980a67f8411e4fa202e4c7a4006a
dayoung <rossenahuh@gmail.com>
2019-05-17 10:47:02 +0900

    complete search function

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import Header from '../Header/Header';
+import Search from '../Search/Search';
+import { Row } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Main() {
   return (
     <Fragment>
       <Header />
+      <Search />
     </Fragment>
   );
 }
